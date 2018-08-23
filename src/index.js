d38fbba8ff1fb80b0754bdee1e90b640f5eb6cf0
Kushal Mahajan <kushal.mahajan@outlook.com>
2018-08-24 02:20:22 +0530

    Revert "code clean up"
    This reverts commit c6a8d6783f59b4b751712315d36364346da3438e.

--- src/index.js
+++ src/index.js
@@ -2,11 +2,27 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './app.js';
 
+const title = 'Minimal React Webpack Babel Setup';
+
+const createRoot = () => {
+    var root = document.createElement('div');
+    root.setAttribute('id', 'root');
+    document.body.appendChild(root);
+}
+createRoot();
 
 ReactDOM.render(
     <App />,
     document.getElementById('root')
 );
-
+// async function getComponent() {
+//     var element = document.createElement('div');
+//     const _ = await import(/* webpackChunkName: "lodash" */ 'lodash');
+//     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+//     return element;
+// }
+// getComponent().then(component => {
+//     document.body.appendChild(component);
+// });
 
 
