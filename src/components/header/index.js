442a8cac5d3cd58628668b5c2a34d6e25306e4ab
Kushal Mahajan <kushal.mahajan@outlook.com>
2018-08-24 12:22:31 +0530

    Added header

---
+++ src/components/header/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import HeaderStyles from './styles';
+
+const header = () => {
+    return (
+        <HeaderStyles>My custom header</HeaderStyles>
+    );
+}
+export default header;
--- src/sample.js
+++ src/sample.js
@@ -1,13 +1,15 @@
 import React from 'react';
-import Header from './components/header/index.js';
-import './style.css';
+import PrefetchExample from './prefetch.js';
+import LazyLoadExample from './lazy_load.js';
 
 class App extends React.Component {
     render() {
         const title = 'Minimal React Webpack Babel Setup';
         return (
             <div>
-                <Header><h3>{title}</h3></Header>
+                <h3>{title}</h3>
+                <PrefetchExample />
+                <LazyLoadExample />
             </div>
         );
     }
