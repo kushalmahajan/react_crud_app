d9cad7fa2536a4e17a3250639b8a2c1f76ec8433
Kushal Mahajan <kushal.mahajan@outlook.com>
2018-08-25 12:38:06 +0530

    static image icons in place, dynamic import removed

--- src/components/list/list_item.js
+++ src/components/list/list_item.js
@@ -3,35 +3,37 @@ import Card from '../../base_components/card/card.js';
 
 import Row from '../../base_components/row.js';
 import Col from '../../base_components/col.js';
-// import male from './male.png';
+import male from './male.png';
+import female from './female.png';
 
-const getIcon = async (gender) => {
-    const placeholder = await import(`./${gender}.png`);
-    return placeholder.default;
-}
+// const getIcon = async (gender) => {
+//     let placeholder = '';
+//     if (gender === 'male') {
+//         placeholder = await import(/* webpackChunkName male */ `./${gender}.png`);
+//     } else {
+//         placeholder = await import(/* webpackChunkName female */ `./${gender}.png`);
+//     }
+
+//     return placeholder.default;
+// }
 const Item = (props) => {
     const { name, height, hair_color, eye_color, gender, url, created } = props.data;
-    const elms = [];
-    getIcon(gender).then(res => {
-        console.log(gender, res);
-        elms.push(
-            <li className='m-b-15'>
-                <Card>
-                    <Row>
-                        <Col sm={4}>
-                            <img width='40' height='40' src={res} />
-                        </Col>
-                        <Col sm={8}>
-                            <h4>{name}</h4>
-                            <p className='text-muted'>
-                                Hello
+    return (
+        <li className='m-b-15'>
+            <Card>
+                <Row>
+                    <Col sm={4}>
+                        <img width='40' height='40' src={gender === 'male' ? male : female} />
+                    </Col>
+                    <Col sm={8}>
+                        <h4>{name}</h4>
+                        <p className='text-muted'>
+                            Hello
                             </p>
-                        </Col>
-                    </Row>
-                </Card>
-            </li>
-        )
-    });
-    return elms;
+                    </Col>
+                </Row>
+            </Card>
+        </li>
+    );
 }
 export default Item;
